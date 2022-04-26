2b347457a6c0ad4551314806ff1eb6276ef60a8d
ferruh <ferruhcihan20@gmail.com>
2022-04-26 23:06:39 +0300

    Add db connection error handling

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb_connect_link")
-  .then(() => console.log("DB Connection is Successful!"));
+  .then(() => console.log("DB Connection is Successful!"))
+  .catch((err) => console.log("DB Connection Error: ", err));
 
 app.listen(5000, () => {
   console.log("Backend server is running!");
