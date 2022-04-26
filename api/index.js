73e58f1b0b09d5b4d43006415a13d7205ed51c44
ferruh <ferruhcihan20@gmail.com>
2022-04-26 23:52:21 +0300

    Update .env and index.js structure

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose
-  .connect("mongodb_connect_link")
+dotenv.config();
+
+main()
   .then(() => console.log("DB Connection is Successful!"))
   .catch((err) => console.log("DB Connection Error: ", err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
 
 app.listen(5000, () => {
   console.log("Backend server is running!");
