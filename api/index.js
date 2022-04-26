9cf40e3c645467a306c04f150f847bc26242cfcf
ferruh <ferruhcihan20@gmail.com>
2022-04-26 22:45:54 +0300

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb_connect_link")
+  .then(() => console.log("DB Connection is Successful!"));
 
 app.listen(5000, () => {
   console.log("Backend server is running!");
