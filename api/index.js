322dc1b82dc797ead3cd7a488092326da216f8de
ferruh <ferruhcihan20@gmail.com>
2022-04-27 23:20:34 +0300

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const host = "127.0.0.1";
+const port = 5000;
 
 dotenv.config();
 
@@ -13,6 +15,11 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
-app.listen(5000, () => {
+// Homepage
+app.get("/", (req, res) => {
+  res.send("E-Commerce App");
+});
+
+app.listen(port, host, () => {
   console.log("Backend server is running!");
 });
