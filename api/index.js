9520cfd7eae37ca9e09c27508117d747b4c2e1a6
ferruh <ferruhcihan20@gmail.com>
2022-04-27 23:23:28 +0300

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -13,11 +13,6 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
-// Homepage
-app.get("/", (req, res) => {
-  res.send("E-Commerce App");
-});
-
-app.listen(process.env.PORT, process.env.HOST, () => {
+app.listen(process.env.PORT || 5000, process.env.HOST, () => {
   console.log("Backend server is running!");
 });
