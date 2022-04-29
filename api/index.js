28356cf0682f93c493bf5bbd40ce89b637cd319b
ferruh <ferruhcihan20@gmail.com>
2022-04-29 23:12:41 +0300

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
-app.use("/user", userRouter);
+app.use("/api/user", userRouter);
 
 app.listen(process.env.PORT || 5000, process.env.HOST, () => {
   console.log("Backend server is running!");
