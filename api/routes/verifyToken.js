957e71ba69559b6bd02363e9ee0c237963162980
ferruh <ferruhcihan20@gmail.com>
2022-05-02 23:08:36 +0300

    Add verifyTokenAndAuthorization

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -14,6 +14,17 @@ const verifyToken = (req, res, next) => {
   }
 };
 
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
 module.exports = {
   verifyToken,
+  verifyTokenAndAuthorization,
 };
