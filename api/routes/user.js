ad0f61114cd74438ea86928e2e501cdab0423396
ferruh <ferruhcihan20@gmail.com>
2022-05-03 23:50:15 +0300

    Add user update func

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
-const { verifyToken } = require("./verifyToken");
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
-router.put("/:id", verifyToken, (res, req) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
-    res.send("Successful!");
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
