5fbb2697632f8a7e4bc14cf024e41a4628ec03a5
ferruh <ferruhcihan20@gmail.com>
2022-05-05 17:29:39 +0300

    Update userRoute

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const {
-  verifyToken,
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
 } = require("./verifyToken");
