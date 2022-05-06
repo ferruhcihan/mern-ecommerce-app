ef0ae431341af005aa459e10f5fbb6d905b175bb
ferruh <ferruhcihan20@gmail.com>
2022-05-06 17:17:24 +0300

    Update createProduct

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,7 +4,6 @@ const router = require("express").Router();
 
 //CREATE
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  console.log("create Product");
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
