f72f69974aa926b612685fc0201a39dcb6275e95
ferruh <ferruhcihan20@gmail.com>
2022-05-06 17:20:47 +0300

    Add deleteProduct func

--- api/routes/product.js
+++ api/routes/product.js
@@ -29,4 +29,14 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
