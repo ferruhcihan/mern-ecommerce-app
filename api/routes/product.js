dafe22a63413611d9feb642757762295da7ae074
ferruh <ferruhcihan20@gmail.com>
2022-05-07 19:29:40 +0300

    Add getProduct func

--- api/routes/product.js
+++ api/routes/product.js
@@ -39,4 +39,14 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
