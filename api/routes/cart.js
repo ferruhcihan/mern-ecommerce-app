a24a68e97b1f9aee7c52509ca3524a6d74dd9a6c
ferruh <ferruhcihan20@gmail.com>
2022-05-08 23:35:19 +0300

    Add cartDelete func

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -31,4 +31,14 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
