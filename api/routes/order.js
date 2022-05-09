6288b11e96625746e193d2d30bc175981254fd1f
ferruh <ferruhcihan20@gmail.com>
2022-05-09 23:08:02 +0300

    Add createOrder func

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,7 +1,18 @@
+const Order = require("../models/Order");
+const { verifyToken } = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Order Router!");
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
