c3182f0a392f820c3e7555da055fe3bb0eb2e2da
ferruh <ferruhcihan20@gmail.com>
2022-05-13 23:08:37 +0300

    Add pages & update structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Pay from "./pages/Pay";
+import Success from "./pages/Success";
+
 function App() {
-  return <div>E-Commerce App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
