d6c6befc43ad1e5e2bdd60025df7a8d09cfe3891
ferruh <ferruhcihan20@gmail.com>
2022-05-16 22:36:44 +0300

    Add Register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Cart from "./pages/cart";
 import Home from "./pages/home";
+import Login from "./pages/login";
 import Pay from "./pages/Pay";
+import Register from "./pages/register";
 import Success from "./pages/Success";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path="/pay" element={<Pay />} />
         <Route path="/success" element={<Success />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
