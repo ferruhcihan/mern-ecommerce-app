5f40b09ba8dcbc9fcd5c0ca743b46fa788193019
ferruh <ferruhcihan20@gmail.com>
2022-05-17 23:04:57 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import Cart from "./pages/cart";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Pay from "./pages/Pay";
+import Product from "./pages/product";
+import ProductList from "./pages/productList";
 import Register from "./pages/register";
 import Success from "./pages/Success";
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/products/:params" element={<ProductList />} />
       </Routes>
     </div>
   );
