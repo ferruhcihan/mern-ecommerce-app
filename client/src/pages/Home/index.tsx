6f5c3b759c8475027911f39114da8b8792d5f814
ferruh <ferruhcihan20@gmail.com>
2022-05-19 19:07:30 +0300

    Add new components

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Announcement from "../../components/announcement";
+import Categories from "../../components/categories";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
+import Newsletter from "../../components/newsletter";
+import Products from "../../components/products";
+import Slider from "../../components/slider";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
