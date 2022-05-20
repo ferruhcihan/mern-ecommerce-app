e7cab7c172d547645723623707526773350410c0
ferruh <ferruhcihan20@gmail.com>
2022-05-20 21:19:52 +0300

    Update Slider comp

--- client/src/components/slider/index.tsx
+++ client/src/components/slider/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div className="relative hidden w-full h-screen overflow-hidden sm:flex ">
+      <div className="flex h-full transition-all duration-300 ease-in-out translate-x-4">
+        wrapper
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
