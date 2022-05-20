c0b501d776c02b7f2c2dd5a53183a10da457775d
ferruh <ferruhcihan20@gmail.com>
2022-05-20 21:26:25 +0300

    Update Slider.js

--- client/src/components/slider/index.tsx
+++ client/src/components/slider/index.tsx
@@ -1,6 +1,11 @@
+import { MdOutlineArrowLeft } from "react-icons/md";
+
 const Slider = () => {
   return (
     <div className="relative hidden w-full h-screen overflow-hidden sm:flex ">
+      <div className="w-12 h-12 bg-[#fff7f7] rounded-lg flex items-center justify-center absolute top-0 bottom-0 m-auto cursor-pointer opacity-50 z-10">
+        <MdOutlineArrowLeft />
+      </div>
       <div className="flex h-full transition-all duration-300 ease-in-out translate-x-4">
         wrapper
       </div>
