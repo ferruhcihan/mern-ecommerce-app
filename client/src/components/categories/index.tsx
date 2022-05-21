9abf8694fcb8da0a0b6c7cb51930de09a05b4ec9
ferruh <ferruhcihan20@gmail.com>
2022-05-21 22:06:27 +0300

    Update Categories comp

--- client/src/components/categories/index.tsx
+++ client/src/components/categories/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import CategoryItem from "./categoryItem";
+
+const categories = [{ id: "category-1" }];
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div>
+      {categories.map((item: any) => (
+        <CategoryItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
