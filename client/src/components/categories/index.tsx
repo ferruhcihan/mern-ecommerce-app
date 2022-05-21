66e6c4c3e698ee1d22855dd743b15817a8b837e2
ferruh <ferruhcihan20@gmail.com>
2022-05-21 22:08:39 +0300

    Update Categories comp

--- client/src/components/categories/index.tsx
+++ client/src/components/categories/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
 import CategoryItem from "./categoryItem";
 
-const categories = [{ id: "category-1" }];
+const categories = [
+  { id: "category-1" },
+  { id: "category-2" },
+  { id: "category-3" },
+];
 
 const Categories = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-between p-0 sm:p-5 sm:flex-row ">
       {categories.map((item: any) => (
         <CategoryItem key={item.id} item={item} />
       ))}
