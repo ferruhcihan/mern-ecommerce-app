e8ea0161a2747311d4f0058e00e8ece1a6970617
ferruh <ferruhcihan20@gmail.com>
2022-05-22 22:01:18 +0300

    Update CategoryItem comp

--- client/src/components/categories/categoryItem.tsx
+++ client/src/components/categories/categoryItem.tsx
@@ -8,7 +8,12 @@ const CategoryItem = ({ item }: any) => {
         alt="categoryItemImg"
         className="w-full h-[20vh] sm:h-full object-cover "
       />
-      <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full"></div>
+      <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full">
+        <h1 className="mb-5 text-white">{item.title}</h1>
+        <button className="p-3 font-semibold text-gray-400 bg-white cursor-pointer">
+          SHOP NOW
+        </button>
+      </div>
     </div>
   );
 };
