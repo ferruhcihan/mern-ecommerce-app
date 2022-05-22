d24b982c7ecfebae439aabff6178b87d9ea2bf2a
ferruh <ferruhcihan20@gmail.com>
2022-05-22 21:58:40 +0300

    Update CategoryItem comp

--- client/src/components/categories/categoryItem.tsx
+++ client/src/components/categories/categoryItem.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const CategoryItem = ({ item }: any) => {
-  return <div>CategoryItem</div>;
+  return (
+    <div className="flex m-1 h-[70vh] relative">
+      <img
+        src={item.img}
+        alt="categoryItemImg"
+        className="w-full h-[20vh] sm:h-full object-cover "
+      />
+      <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full"></div>
+    </div>
+  );
 };
 
 export default CategoryItem;
