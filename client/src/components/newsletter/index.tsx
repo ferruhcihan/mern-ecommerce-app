0d087d3448e65d15bb1f9508221cbdf3fc0f4567
ferruh <ferruhcihan20@gmail.com>
2022-05-23 23:08:31 +0300

    Update Newsletter comp

--- client/src/components/newsletter/index.tsx
+++ client/src/components/newsletter/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className="h-[60vh] bg-[#fcf5f5] flex flex-col justify-center items-center">
+      <h1 className="mb-5 text-7xl">Newsletter</h1>
+      <div className="flex"></div>
+    </div>
+  );
 };
 
 export default Newsletter;
