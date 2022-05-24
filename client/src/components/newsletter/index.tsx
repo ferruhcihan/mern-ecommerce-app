54b86b024b14f9b75e4f589c394883d26db2ffa6
ferruh <ferruhcihan20@gmail.com>
2022-05-24 23:15:52 +0300

    Update Newsletter comp

--- client/src/components/newsletter/index.tsx
+++ client/src/components/newsletter/index.tsx
@@ -4,7 +4,9 @@ const Newsletter = () => {
   return (
     <div className="h-[60vh] bg-[#fcf5f5] flex flex-col justify-center items-center">
       <h1 className="mb-5 text-7xl">Newsletter</h1>
-      <div className="flex"></div>
+      <div className="mb-5 text-2xl font-light text-center sm:text-left">
+        Get timely updates from your favorite products.
+      </div>
     </div>
   );
 };
