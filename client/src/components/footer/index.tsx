24de6e3dd78292ce13a11c6315db95a27fdd23da
ferruh <ferruhcihan20@gmail.com>
2022-05-24 23:17:04 +0300

    Update Footer comp

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-col sm:flex-row">
+      <div className="flex"></div>
+    </div>
+  );
 };
 
 export default Footer;
