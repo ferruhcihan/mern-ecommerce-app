c05bd7299d06385ddfc47f690cf006035017b176
ferruh <ferruhcihan20@gmail.com>
2022-05-26 21:30:04 +0300

    Update Footer comp

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -3,7 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex flex-col sm:flex-row">
-      <div className="flex"></div>
+      {/* LEFT */}
+      <div className="flex flex-col flex-1 p-5">
+        <h1>FC</h1>
+        <p className="my-5">
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
+        </p>
+        <div className="flex"></div>
+      </div>
+      {/* RIGHT */}
     </div>
   );
 };
