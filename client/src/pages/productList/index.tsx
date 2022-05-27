71ff022be7bad06d8f22600a3a373a3cb59d47c0
ferruh <ferruhcihan20@gmail.com>
2022-05-27 23:05:00 +0300

    Update ProductList page

--- client/src/pages/productList/index.tsx
+++ client/src/pages/productList/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className="">
+      <h1 className="m-5">ProductList</h1>
+    </div>
+  );
 };
 
 export default ProductList;
