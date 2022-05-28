b8456a84abbc57eec772ef8e83e791bba9305618
ferruh <ferruhcihan20@gmail.com>
2022-05-28 23:06:27 +0300

    Update Login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <div className="flex w-screen h-screen">
-      <div className="w-3/4 p-5 bg-white sm:w-1/4"></div>
+      <div className="w-3/4 p-5 bg-white sm:w-1/4">Login</div>
     </div>
   );
 };
