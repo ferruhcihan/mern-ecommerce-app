76d9be4b9d9e8e753bc18c735f293b5b4952914d
ferruh <ferruhcihan20@gmail.com>
2022-05-28 23:06:37 +0300

    Update Register page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="flex w-screen h-screen">
+      <div className="w-3/4 p-5 bg-white sm:w-1/4">Register</div>
+    </div>
+  );
 };
 
 export default Register;
