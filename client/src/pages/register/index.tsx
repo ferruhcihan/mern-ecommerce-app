61fb199c38f257fd3b2daf1b4f2bf0e272803e33
ferruh <ferruhcihan20@gmail.com>
2022-05-31 21:21:22 +0300

    Update Register page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -3,7 +3,18 @@ import React from "react";
 const Register = () => {
   return (
     <div className="flex w-screen h-screen">
-      <div className="w-3/4 p-5 bg-white sm:w-1/4">Register</div>
+      <div className="w-3/4 p-5 bg-white sm:w-1/4">
+        <h1 className="text-2xl font-light">CREATE AN ACCOUNT</h1>
+        <form className="flex flex-wrap">
+          <span className="my-5 text-sm">
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </span>
+          <button className="w-2/5 px-5 py-4 text-white cursor-pointer bg-teal">
+            CREATE
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
