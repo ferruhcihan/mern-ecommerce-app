0389e22663f8adddf15e83502153b133819ce599
ferruh <ferruhcihan20@gmail.com>
2022-06-01 20:39:39 +0300

    Add Input comp

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,10 @@
+const Input = ({ placeholder }: any) => {
+  return (
+    <input
+      placeholder={placeholder}
+      className="flex flex-1 p-2 mt-5 mr-2 min-w-2/5"
+    />
+  );
+};
+
+export default Input;
