ee23150773e824de155eb2451da707d72e039a75
ferruh <ferruhcihan20@gmail.com>
2022-06-01 20:31:51 +0300

    Add Login page background image

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="flex w-screen h-screen">
+    <div
+      className="flex w-screen h-screen"
+      style={{
+        background:
+          "linear-gradient(#FFFFFF80,#FFFFFF80), url('https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') center",
+      }}
+    >
       <div className="w-3/4 p-5 bg-white sm:w-1/4">Login</div>
     </div>
   );
